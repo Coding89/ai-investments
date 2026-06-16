ddb5fc1c894f318087352452503f2a24876c2af6
Coding89 <timothy.lefevre1@gmail.com>
2026-06-16 09:32:14 +0100

    Updated main script

--- ai-investments.py
+++ ai-investments.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-#Initialise SQLite Database Engine
-engine = create_engine('sqlite:///:memory:', echo=False)
+#Initialise SQLite Database Engine and creates a permanent SQLite Database file
+engine = create_engine('sqlite:///ai_investments.db', echo=False)
 
 #Loads CSV files
 df_prices = pd.read_csv('ai_financial_market_data.csv')
@@ -10,7 +10,7 @@ df_prices = pd.read_csv('ai_financial_market_data.csv')
 df_companies = pd.read_csv('ai_financial_market_data.csv')
 
 #cleans the data by converting data strings to datetime etc
-df_prices.column = [
+df_prices.columns = [
     'date',
     'company',
     'rd_spending_usd_mn',
@@ -29,3 +29,13 @@ df_prices.to_sql(
 )
 
 print("SQL Database successfully created. Table created called 'ai_financial_data'.")
+
+#This queires and displays the results to verify they saved correctly
+print("--- Fetching the first 10 rows from SQL ---")
+with engine.connect() as connection:
+    query = text("SELECT * FROM ai_financial_data LIMIT 10;")
+    results = connection.execute(query)
+    
+    #formats and prints out each row
+    for row in results:
+        print(row)
