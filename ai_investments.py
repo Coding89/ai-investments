7d9a2577bf5faba015479962bdfc565b6d0a67c3
Coding89 <timothy.lefevre1@gmail.com>
2026-06-25 11:40:05 +0100

    intro edited

--- ai_investments.py
+++ ai_investments.py
@@ -1,17 +1,16 @@
 """
 AI Investments Data Pipeline
----------------------------
+
 This script automates the process of loading AI financial market datasets from local CSV files.
 The open source data is obtained from Kaggle. It stages and cleans the column schemas and streams
-the processed data directly into a local and permamnent SQlite database file.
+the processed data directly into a local and permanent SQlite database file.
 
-Additionally, the script alo includes verification layers that connect to the final databse, executes relational SQL queries
+Additionally, the script alo includes verification layers that connect to the final database, executes relational SQL queries
 and automates summary statistics.
 
-Note that data from Kaggle is not always accurate.
+Note that data from Kaggle is not always accurate, so please keep an eye out on the schema validation if it breaks + how accurate is this data?
 
-Script uses:
-- pandas, sqlalchemy
+Dependencies: pandas, sqlalchemy
 
 """
 import sys
@@ -26,7 +25,7 @@ except FileNotFoundError:
     sys.exit(1)
 
 # Maps raw schema to standardise naming
-#TODO: move this to a config file if we are more datasets in future
+#TODO: move this to a config file if we have more datasets in the future
 df.columns = [
     'date', 'company', 'rd_spending_usd_mn', 'ai_revenue_usd_mn', 'ai_revenue_growth_pct', 'event', 'stock_impact_pct'
 ]
